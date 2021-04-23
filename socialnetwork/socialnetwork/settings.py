bd48a9c2647ab110a6682bdf98f68e3ee2c20158
Kashish srivastava <44269697+cannibalcheeseburger@users.noreply.github.com>
2021-04-23 19:24:02 +0530

    asd

--- socialnetwork/socialnetwork/settings.py
+++ socialnetwork/socialnetwork/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,7 +128,7 @@ AUTH_USER_MODEL = 'agenda.users'
 
 import os.path
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
 
