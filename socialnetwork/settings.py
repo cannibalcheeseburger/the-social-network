1dad01bfaa182bbf933e0f59c0ec828d69e88fb1
Kashish srivastava <44269697+cannibalcheeseburger@users.noreply.github.com>
2021-04-23 20:28:53 +0530

    clzkhxc

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@pun21ky&=^o^90n40a_kh49kwp1j_yitc8o*nuwa@nvz(=&9%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['countrymen.herokuapp.com','127.0.0.1']
 
