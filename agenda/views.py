01f3e56fb0cfb5bc7d8b586452eb9bce5a8b7bdb
Akash Rana <akashrana20022000@gmail.com>
2021-04-28 09:23:25 +0530

    gonna merge to master

--- agenda/views.py
+++ agenda/views.py
@@ -170,33 +170,33 @@ def login_user(request):
 """
 def login_user(request):
     if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
+        form_l = LoginForm(request.POST)
+        if form_l.is_valid():
+            username = form_l.cleaned_data['username']
+            password = form_l.cleaned_data['password']
             user = authenticate(username=username, password=password)           
             if user is not None:
                 login(request, user)
                 return redirect('home')
-    form = LoginForm()
-    return render(request, 'login.html',{'form':form})
+    form_l = LoginForm()
+    return render(request, 'login.html',{'form_l':form_l})
 
 def register_user(request):
     if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            email = form.cleaned_data['email']
+        form_r = RegisterForm(request.POST)
+        if form_r.is_valid():
+            username = form_r.cleaned_data['username']
+            email = form_r.cleaned_data['email']
 
-            password1 = form.cleaned_data['password1']
-            password2 = form.cleaned_data['password2']
+            password1 = form_r.cleaned_data['password1']
+            password2 = form_r.cleaned_data['password2']
             if password1 == password2:
                 password = make_password(password1)
                 user = Users(username=username,email=email,password=password)
                 user.save()
                 return redirect('login')
-    form = RegisterForm()
-    return render(request, 'register.html',{'form':form})
+    form_r = RegisterForm()
+    return render(request, 'register.html',{'form_r':form_r})
 
 
 def Logout(request):
